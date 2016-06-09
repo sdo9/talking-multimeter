54e55476bd23562e69cb0a3572f537c6648215f3
Nicolas Pitre <nico@fluxnic.net>
2016-06-09 13:05:09 -0400

    don't fiddle with PWM regs needlessly

--- dmm-talking-arduino-sketch/sketch.ino
+++ dmm-talking-arduino-sketch/sketch.ino
@@ -249,17 +249,18 @@ void play(utter_t utt) {
   /* we consumed top nibble, make sure nextNibble is non zero */
   nextNibble |= 0x80;
 
-  isPlaying = 1;
-
-  digitalWrite(AMP_ENABLE_PIN, HIGH);
-  noInterrupts();
   playLen = len;
 
-  ICR1 = PWM_SCALE;
-  TCCR1A = _BV(WGM11) | _BV(COM1A1); //WGM11,12,13 all set to 1 = fast PWM/w ICR TOP
-  TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS10);
-  TIMSK1 = _BV(TOIE1);
-  interrupts();
+  if (!isPlaying) {
+    isPlaying = 1;
+    digitalWrite(AMP_ENABLE_PIN, HIGH);
+    noInterrupts();
+    ICR1 = PWM_SCALE;
+    TCCR1A = _BV(WGM11) | _BV(COM1A1); //WGM11,12,13 all set to 1 = fast PWM/w ICR TOP
+    TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS10);
+    TIMSK1 = _BV(TOIE1);
+    interrupts();
+  }
 }
 
 void stopPlay() {
